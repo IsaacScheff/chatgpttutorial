ce6ea59c0b52ed11f4df1017e30c62e3c096da56
Isaac Scheff <isaac1996@comcast.net>
2024-03-06 22:48:34 -0500

    Succesfully tested API with hard coded query

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-require('dotenv').config();
-
-import { Configuration, OpenAIApi } from "openai";
+import 'dotenv/config';
+import OpenAI from "openai";
 import readline from "readline";
 
 const openaiOrg = process.env.OPENAI_ORG;
 const openaiApiKey = process.env.OPENAI_API_KEY;
 
-const configuration = new Configuration({
+const openai = new OpenAI({
     organization: openaiOrg,
     apiKey: openaiApiKey,
 });
+
+openai
+  .chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello" }],
+  })
+  .then((response) => {
+    // Attempt to directly access the message content
+    const messageContent = response.choices[0].message.content;
+
+    console.log(messageContent);
+  })
+  .catch((e) => {
+    console.log('Error:', e);
+  });
